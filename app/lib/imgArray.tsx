8524249e984b90141c441ccb76a096d60782e08e
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-09 17:37:28 +0200

    feat: new image array now containing an src and a title

---
+++ app/lib/imgArray.tsx
@@ -0,0 +1,27 @@
+// epxort an "images" object array with src and title properties. the src will be a url to an unsplash image. the aspect ratio of the images should be very varied
+export const images = [
+  {
+    src: "https://images.unsplash.com/photo-1757377125320-cf0d54c0c6e8",
+    title: "cityscape",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+    title: "mountains",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1494526585095-c41746248156",
+    title: "forest",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1534081333815-ae5019106622",
+    title: "desert",
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1757260019137-0d1e8389c9d3",
+    title: "field",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1751517298153-9987060ba831",
+    title: "waves",
+  },
+];
