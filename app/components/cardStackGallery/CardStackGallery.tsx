e161358be51647d53eec91ca98d2faf5fa0420ab
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-16 13:45:45 +0200

    feat: added proper bg color

--- app/components/cardStackGallery/CardStackGallery.tsx
+++ app/components/cardStackGallery/CardStackGallery.tsx
@@ -2,7 +2,7 @@ import images, { StackImagesArray } from "../../lib/cardStackGallery/fetchStackI
 import CardStackCanvas from "./CardStackCanvas/CardStackCanvas";
 
 // Main Component Export
-export default async function CardStack() {
+export default async function CardStackGallery() {
   let imageArray: StackImagesArray = [];
   try {
     imageArray = await images();
@@ -14,7 +14,14 @@ export default async function CardStack() {
   if (!imageArray || imageArray.length === 0) return null;
 
   return (
-    <div className="w-full h-screen overflow-hidden touch-none">
+    <div
+      className="w-full h-screen overflow-hidden touch-none"
+      style={{
+        background:
+          "linear-gradient(167deg, rgba(18, 18, 18, 0.00) 50%, rgba(18, 18, 18, 0.05) 92.09%), linear-gradient(165deg, rgba(252, 252, 252, 0.30) 9.01%, rgba(252, 252, 252, 0.05) 90.99%), rgba(252, 252, 252, 1)",
+        backgroundBlendMode: "plus-darker, normal, normal",
+      }}
+    >
       <CardStackCanvas images={imageArray} />
     </div>
   );
