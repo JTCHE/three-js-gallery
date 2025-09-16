9ff9080088bfd0bf43a29ce568de85718406f1f3
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-16 13:46:08 +0200

    feat: canvas component is now only a canvas element, not nested under a div anymore

--- app/components/cardStackGallery/CardStackCanvas/CardStackCanvas.tsx
+++ app/components/cardStackGallery/CardStackCanvas/CardStackCanvas.tsx
@@ -5,20 +5,17 @@ import CardStackScene from "./CardStackScene/CardStackScene";
 
 export default function CardStackCanvas({ images }: { images: StackImagesArray }) {
   if (!images || images.length === 0) return null;
+
   return (
-    <Canvas
-      className="w-full h-full"
-      gl={{
-        antialias: true,
-        alpha: false,
-      }}
-      style={{ touchAction: "none" }}
-    >
-      <color
-        attach="background"
-        args={["white"]}
-      />
-      <CardStackScene images={images} />
-    </Canvas>
+
+      <Canvas
+        className="w-full h-full"
+        gl={{
+          antialias: true,
+        }}
+        style={{ touchAction: "none", width: "100%", height: "100%" }}
+      >
+        <CardStackScene images={images} />
+      </Canvas>
   );
 }
