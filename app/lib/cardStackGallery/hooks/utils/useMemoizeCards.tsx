44a15477aeb964eea441ad05308a2023b64f0de5
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-20 21:35:10 +0200

    feat: new variables to control whether the full resolution image should be loaded or not, based on the current scroll position

--- app/lib/cardStackGallery/hooks/utils/useMemoizeCards.tsx
+++ app/lib/cardStackGallery/hooks/utils/useMemoizeCards.tsx
@@ -1,35 +1,42 @@
 import { CardProps } from "@/app/components/cardStackGallery/CardStackCanvas/CardStackScene/Card/Card";
 import { useMemo } from "react";
-import * as THREE from "three";
 import { StackImagesArray } from "../../fetchStackImages";
 
 export default function useMemoizeCards({
   scrollPosition,
   hoveredIndex,
-  hoverTimeout,
   imageCount,
   images,
-  renderDistance,
   spacing,
-  camera,
 }: {
   scrollPosition: number;
   hoveredIndex: number | null;
-  hoverTimeout: React.RefObject<NodeJS.Timeout | null>;
   imageCount: number;
   images: StackImagesArray;
-  renderDistance: number;
   spacing: number;
-  camera: THREE.Camera;
 }) {
+  const renderDistance = 7;
+
   return useMemo(() => {
     const centerIndex = Math.round(scrollPosition);
     const cardData: Array<CardProps> = [];
 
     for (let i = centerIndex - renderDistance; i <= centerIndex + renderDistance; i++) {
       const imageIndex = ((i % imageCount) + imageCount) % imageCount;
+
+      // Determine current card distance from center index
+      const distanceFromCenter = Math.abs(i - centerIndex);
+
+      // Determine what to load based on distance
+      const immediateLoadDistance = 3; // Load full images within 3 cards, starting from center, backwards and forwards (7 total)
+      const shouldLoadFullImage = distanceFromCenter <= immediateLoadDistance;
+      const isInRenderDistance = distanceFromCenter <= renderDistance;
+
       cardData.push({
         cardIndex: i,
+        imageWidth: images[imageIndex].imageWidth,
+        imageHeight: images[imageIndex].imageHeight,
+        placeholderUrl: images[imageIndex].placeholderSrc,
         thumbnailUrl: images[imageIndex].thumbnailSrc,
         snippetUrl: images[imageIndex].snippetSrc,
         cardTitle: images[imageIndex].title,
@@ -37,9 +44,11 @@ export default function useMemoizeCards({
         isActive: hoveredIndex !== null ? i === hoveredIndex : i === centerIndex,
         cardOwnerTitle: images[imageIndex].ownerTitle,
         cardOwnerSlug: images[imageIndex].ownerSlug,
+        shouldLoadFull: shouldLoadFullImage,
+        isInRenderDistance,
         onClick: () => void 0, // Placeholder, will be set in CardStackScene
       });
     }
     return cardData;
-  }, [Math.round(scrollPosition), hoveredIndex, hoverTimeout, imageCount, images, renderDistance, spacing, camera]);
+  }, [scrollPosition, hoveredIndex, imageCount, images, spacing, renderDistance]);
 }
