7c2a64189c02145aae40e91dfe5aa507d31e1ed8
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-20 21:34:36 +0200

    fix: added missing dependencies to the array

--- app/lib/cardStackGallery/hooks/gestures/touch/useTouchStart.tsx
+++ app/lib/cardStackGallery/hooks/gestures/touch/useTouchStart.tsx
@@ -10,30 +10,33 @@ export default function useTouchStart(
   lastTouchTime: React.RefObject<number>,
   gl: { domElement: HTMLElement }
 ) {
-  return useCallback((event: MouseEvent | TouchEvent) => {
-    if (isDragging.current) {
+  return useCallback(
+    (event: MouseEvent | TouchEvent) => {
+      if (isDragging.current) {
+        event.preventDefault();
+        return;
+      }
+
       event.preventDefault();
-      return;
-    }
 
-    event.preventDefault();
+      let clientY, clientX;
+      if ("touches" in event && event.touches.length > 0) {
+        clientY = event.touches[0].clientY;
+        clientX = event.touches[0].clientX;
+      } else {
+        clientY = (event as MouseEvent).clientY;
+        clientX = (event as MouseEvent).clientX;
+      }
 
-    let clientY, clientX;
-    if ("touches" in event && event.touches.length > 0) {
-      clientY = event.touches[0].clientY;
-      clientX = event.touches[0].clientX;
-    } else {
-      clientY = (event as MouseEvent).clientY;
-      clientX = (event as MouseEvent).clientX;
-    }
-
-    touchStartY.current = clientY;
-    touchStartX.current = clientX;
-    lastTouchY.current = clientY;
-    isDragging.current = true;
-    velocity.current = 0;
-    touchVelocity.current = 0;
-    lastTouchTime.current = performance.now();
-    gl.domElement.style.cursor = "grabbing";
-  }, []);
+      touchStartY.current = clientY;
+      touchStartX.current = clientX;
+      lastTouchY.current = clientY;
+      isDragging.current = true;
+      velocity.current = 0;
+      touchVelocity.current = 0;
+      lastTouchTime.current = performance.now();
+      gl.domElement.style.cursor = "grabbing";
+    },
+    [gl.domElement.style, isDragging, lastTouchTime, lastTouchY, touchStartX, touchStartY, touchVelocity, velocity]
+  );
 }
