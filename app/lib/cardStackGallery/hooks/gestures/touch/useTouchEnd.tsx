b9723ac5a6d2660126145e25d96504cfc539d637
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-09-21 12:06:59 +0200

    chore: formatted code

--- app/lib/cardStackGallery/hooks/gestures/touch/useTouchEnd.tsx
+++ app/lib/cardStackGallery/hooks/gestures/touch/useTouchEnd.tsx
@@ -6,13 +6,16 @@ export default function useTouchEnd(
   velocity: React.RefObject<number>,
   gl: { domElement: HTMLElement }
 ) {
-  return useCallback((event: TouchEvent | MouseEvent) => {
-    event.preventDefault();
-    if (isDragging.current) {
-      velocity.current = touchVelocity.current * 0.1;
-      velocity.current = Math.max(-3, Math.min(3, velocity.current));
-    }
-    gl.domElement.style.cursor = "grab";
-    isDragging.current = false;
-  }, [gl.domElement.style, isDragging, touchVelocity, velocity]);
+  return useCallback(
+    (event: TouchEvent | MouseEvent) => {
+      event.preventDefault();
+      if (isDragging.current) {
+        velocity.current = touchVelocity.current * 0.1;
+        velocity.current = Math.max(-3, Math.min(3, velocity.current));
+      }
+      gl.domElement.style.cursor = "grab";
+      isDragging.current = false;
+    },
+    [gl.domElement.style, isDragging, touchVelocity, velocity]
+  );
 }
